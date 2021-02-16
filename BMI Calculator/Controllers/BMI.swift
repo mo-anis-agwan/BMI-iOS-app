5b0626c48039a684ecdacc0b68c1131f7edb02a3
anis-agwan <anishagwan@gmail.com>
2021-02-17 01:42:33 +0530

    Body Mass Index calculator

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Anis Agwan on 19/01/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    var value: Float
+    var advice: String
+    var color: UIColor
+}
